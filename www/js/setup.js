0304f03dbca2ce77f04d5f0682f5e5023cb23784
Ash Blue <ash@ashbluewebdesign.com>
2012-09-29 16:45:00 -0500

    Fix for crash on player

--- www/js/setup.js
+++ www/js/setup.js
@@ -12,12 +12,12 @@
     // Width, height, and game run logic
     cp.core.init(1024, 768, function () {
         // Keyboard key configuration
-        cp.input.bind('arrowUp', 'up');
-        cp.input.bind('arrowDown', 'down');
-        cp.input.bind('arrowLeft', 'left');
-        cp.input.bind('arrowRight', 'right');
+        //cp.input.bind('arrowUp', 'up');
+        //cp.input.bind('arrowDown', 'down');
+        //cp.input.bind('arrowLeft', 'left');
+        //cp.input.bind('arrowRight', 'right');
 
-        cp.game.spawn('ActivePlayer');
+        cp.game.spawn('Level');
         //cp.game.spawn('RemotePlayer');
     });
 }(cp));
