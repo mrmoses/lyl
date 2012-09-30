91810e424f60f6edf748e1a28bf71c88a90ee0d3
Ash Blue <ash@ashbluewebdesign.com>
2012-09-30 05:25:09 -0500

    Added the game over base entity and large upgrade to player's logic

---
+++ www/js/objects/game/game-over.js
@@ -0,0 +1,28 @@
+/**
+ * @requires models/leveldata.js
+ */
+(function (cp) {
+    cp.template.GameOver = cp.template.Entity.extend({
+        name: 'gameOver',
+        x: 453,
+        y: 336,
+        width: 100,
+        height: 100
+
+        //init: function () {
+        //
+        //},
+
+        //setType: function () {
+        //
+        //}
+
+        //update: function () {
+        //    return;
+        //},
+        //
+        //draw: function () {
+        //    return;
+        //},
+    });
+}(cp));
