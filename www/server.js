e105e2fd8c16f186d07f2ad3b4c065aded7b92ae
mrmoses <jnmoses@gmail.com>
2012-10-02 20:24:48 -0500

    added labels to user list, disconnect all clients on lemmegeddon

--- www/server.js
+++ www/server.js
@@ -42,22 +42,21 @@ var server = {
      */
     init: function () {
         SELF = this;
-        
+
         //updated according to https://github.com/visionmedia/express/wiki/Migrating-from-2.x-to-3.x
         this.app = this.express();
         this
             .setFolders()
             .setReturnJSON('images', ['.jpg', '.png', '.gif'], '/include/image-files.php')
             .getAudio('audio', '/include/sound-files.php');
-            
+
         this.auth = this.express.basicAuth('user', 'pass');
-        this.app.get('/admin', this.auth, function(req, res) { 
+        this.app.get('/admin', this.auth, function(req, res) {
 			res.sendfile(__dirname + '/admin.html');
 		});
 
         this.server = this.http.createServer(this.app).listen(PORT);
-        console.log('listening on ' + PORT);
-        
+
         this.app.get('/arena', function (req, res) {
 			res.sendfile(__dirname + '/arena.html');
 		});
@@ -75,7 +74,6 @@ var server = {
     setFolders: function () {
         // Activates production mode so you can demo the compiled code
         this.app.configure('production', function () {
-            console.log('production');
 
             SELF.app.get('/js/all.js', function (req, res) {
                 var compiledJS = SELF.compiler.createJS();
@@ -156,7 +154,6 @@ var server = {
             }
         });
 
-        //console.log(filteredContents);
         this.app.get(request, function (req, res) {
             var compiledJS = _files.getCombinedFiles(_jsBuildOrder, ['js']);
             //var compiledJS = '';
@@ -231,6 +228,9 @@ var server = {
 
 var app = server.init();
 
+(function() {
+
+
 /* Multiplayer Dragons be here */
 var player1 = '';
 var player2 = '';
@@ -242,21 +242,24 @@ io = require('socket.io').listen(app);
 io.set('log level', 1);
 io.sockets.on('connection', function (socket) {
 	this.clientId = socket.id;
-	clients[this.clientId] = socket;
-	
-	console.log("added " + this.clientId + " to clients");
-	io.sockets.emit('admin-new-socket', {id: socket.id});
-	
+
+	clients[this.clientId] = {
+		socket: socket,
+	};
+
 	// sends message to client to let them know they are connected
 	socket.emit('clientlog', {msg:"sockets are socketized"});
-	
+
 	socket.on('user-connect', function(data) {
 		this.clientId = data.id;
 		//if player 1 is available
 		if(!player1 || this.clientId == player1) {
 			player1 = this.clientId;
 			io.sockets.emit('adminlog', {msg: "player 1 connected " + player1});
-			
+
+			io.sockets.emit('admin-new-socket', {id: this.clientId, name: 'player1'});
+			clients[this.clientId].name = 'player';
+
 	  		// add the player to the entites
 			entities[this.clientId] = {
 				id: player1,
@@ -264,41 +267,47 @@ io.sockets.on('connection', function (socket) {
 				x:100,
 				y:100
 			};
-			
+
 			//spawn this on already connected screens
 	  		socket.broadcast.emit('spawn-remote-player', entities[this.clientId]);
-			
+
 		//else if player 2 is available
 		} else if (!player2 || this.clientId == player2) {
 			player2 = this.clientId;
 			io.sockets.emit('adminlog', {msg: "player 2 connected " + player2});
-			
+
+			io.sockets.emit('admin-new-socket', {id: this.clientId, name: 'player2'});
+			clients[this.clientId].name = 'player2';
+
 			// add the player to the entites
 			entities[this.clientId] = {
 				id: player2,
 				type:'player',
-				x:1024-100,
-				y:768-100
+				x:1024-200,
+				y:768-200
 			};
-			
+
 			//spawn this on already connected screens
 	  		socket.broadcast.emit('spawn-remote-player', entities[this.clientId]);
-			
+
 		//otherwise they are just a viewer
 		} else {
 			io.sockets.emit('adminlog', {msg: "viewer connected"});
+
+			io.sockets.emit('admin-new-socket', {id: this.clientId, name: 'viewer'});
+			clients[this.clientId].name = 'viewer';
 			socket.emit('init-spectator');
 		}
-		
+
 		//spawn all available entities
 		for(id in entities) {
 			//if this is the active player
-			if(id == this.clientId) {		
+			if(id == this.clientId) {
 				socket.emit('spawn-active-player', entities[id]);
 			} else {
 				socket.emit('spawn-remote-player', entities[id]);
 			}
-		}	
+		}
 	});
 
 	// when a entity is updated on an active client, send data to other clients
@@ -319,27 +328,27 @@ io.sockets.on('connection', function (socket) {
     	for(var id in entities) {
     		io.sockets.emit('entity-kill', entities[id]);
     	}
-  		
+    	entities = {};
+
   		if(data.id == player1) {
 			io.sockets.emit('adminlog', {msg: "player 1 won"});
-	  		clients[player2].emit('game-lost');
-	  		clients[player2].disconnect();
+	  		clients[player2].socket.emit('game-lost');
+	  		clients[player2].socket.disconnect();
   		} else {
 			io.sockets.emit('adminlog', {msg: "player 2 won"});
-	  		clients[player1].emit('game-lost');
-	  		clients[player1].disconnect();
+	  		clients[player1].socket.emit('game-lost');
+	  		clients[player1].socket.disconnect();
   		}
-    	
+
     	socket.emit('game-won');
     	socket.disconnect();
-    	
+
 		io.sockets.emit('game-over');
 
   		delete clients[player1];
   		delete clients[player2];
     	player1 = false;
     	player2 = false;
-    	entities = {};
   	});
 
 	// when a entity is updated on an active client, send data to other clients
@@ -348,41 +357,53 @@ io.sockets.on('connection', function (socket) {
     	for(var id in entities) {
     		io.sockets.emit('entity-kill', entities[id]);
     	}
+    	entities = {};
+
+  		delete clients[player1];
+  		delete clients[player2];
     	player1 = false;
     	player2 = false;
-    	entities = {};
+
 		io.sockets.emit('adminlog', {msg: "All are dead. Hope you're happy."});
-		io.sockets.emit('lemmegeddon', entities[id]);
+		io.sockets.emit('lemmegeddon');
+		for(var id in clients) {
+			clients[id].socket.disconnect();
+		}
   	});
 
-	// when a entity is updated on an active client, send data to other clients
+	// when a admin disconnects a client
   	socket.on('admin-close', function (data) {
-  		clients[data.id].disconnect();
-  		delete clients[data.id]
+		if(clients[data.id]) {
+			clients[data.id].socket.disconnect();
+			delete clients[data.id];
+		}
   	});
 
   	socket.on('admin-connect', function (data, fn) {
 		io.sockets.emit('adminlog', {msg: "admin connect"});
+		io.sockets.emit('admin-new-socket', {id: socket.id, name: 'admin'});
+		clients[socket.id].name = 'admin';
 		var data = {};
-		for(id in clients) {
-			data[id] = id;
+		for(var id in clients) {
+			data[id] = {id: id, name: clients[id].name};
 		}
 		fn(data);
   	});
-	
+
 	/* when a client disconnects */
   	socket.on('disconnect', function () {
 		io.sockets.emit('admin-closed-socket', {id: socket.id});
-		
+		delete clients[socket.id];
+
 		//io.sockets.emit('user disconnected');
   		if(socket.id == player1) {
   			io.sockets.emit('adminlog', {msg: "player 1 disconnected"});
-			io.sockets.emit('entity-kill', entities[id]);
+			io.sockets.emit('entity-kill', entities[player1]);
   			delete entities[player1];
   			player1 = false;
   		} else if(socket.id == player2) {
   			io.sockets.emit('adminlog', {msg: "player 2 disconnected"});
-			io.sockets.emit('entity-kill', entities[id]);
+			io.sockets.emit('entity-kill', entities[player2]);
   			delete entities[player2];
   			player2 = false;
   		} else {
@@ -390,4 +411,5 @@ io.sockets.on('connection', function (socket) {
   		}
   	});
 });
+})();
 /* */
